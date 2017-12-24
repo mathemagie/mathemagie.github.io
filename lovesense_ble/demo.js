1949108b0927e32ebf4bfdbac966cb6086fbe9a0
aurelien fache <aurelien.fache@gmail.com>
2017-12-24 11:36:04 +0100

    add pusher

--- lovesense_ble/demo.js
+++ lovesense_ble/demo.js
@@ -1,5 +1,7 @@
 var devices = [];
 
+var dev;
+
 var init = function() {
   var connectBtn = document.getElementById('connect');
   connectBtn.addEventListener('click', function(event) {
@@ -12,8 +14,30 @@ var init = function() {
 
   var slider = document.getElementById('speed');
   slider.addEventListener('input', function(event) {
+   // console.log(devices);
     devices.forEach(dev => {
       dev.vibrate(parseInt(slider.value,10));
     });
   });
+
+
+// Enable pusher logging - don't include this in production
+Pusher.logToConsole = true;
+
+var pusher = new Pusher('77379ee80f6901a0f5fa', {
+  cluster: 'eu',
+  encrypted: true
+});
+
+var channel = pusher.subscribe('my-channel');
+channel.bind('my-event', function(data) {
+  console.log(data.message);
+  //dev.vibrate(parseInt(10,10));
+   devices.forEach(dev => {
+      dev.vibrate(parseInt(10,10));
+    });
+});
+
 };
+
+
