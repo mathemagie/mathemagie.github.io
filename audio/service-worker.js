aaf09afd1c096f5ac6a45ff63e8662d3f4042d12
aurelien.fache <aurelien.fache@saflab.io>
2024-01-03 13:55:08 +0100

    refresh styles.css

--- audio/service-worker.js
+++ audio/service-worker.js
@@ -2,10 +2,7 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
       caches.open('audio-store').then((cache) => cache.addAll([
         'index.html',
-        'styles.css',
         'app.js',
-        'output.mp3',
-        'background-music.mp3',
       ])),
     );
   });
