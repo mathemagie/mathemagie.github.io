77eead705808916a096e7c5517310ff6bece900b
aurelien.fache <aurelien.fache@saflab.io>
2024-01-03 12:07:36 +0100

    load tracks from json file

--- audio/app.js
+++ audio/app.js
@@ -1,7 +1,19 @@
 const audioPlayer = document.getElementById('audioPlayer');
 let trackIndex = 0;
-const tracks = ['output.mp3', 'background-music.mp3','https://audiod.s3.eu-west-3.amazonaws.com/19721-21.12.2022-ITEMA_23181447-2022F33264E0017-21.mp3'];
+const tracks = [];
 
+function loadTracksFromJson(jsonFile) {
+    fetch(jsonFile)
+        .then(response => response.json())
+        .then(data => {
+            tracks.push(...data.tracks);
+        })
+        .catch(error => {
+            console.error('Error loading tracks:', error);
+        });
+}
+
+loadTracksFromJson('tracks.json');
 function prevTrack() {
     trackIndex--;
     if (trackIndex < 0) {
