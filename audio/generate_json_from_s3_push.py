5d42b084e238a2f31e54789713871e250eac267a
aurelien.fache <aurelien.fache@saflab.io>
2024-01-03 12:31:51 +0100

    Add python script to generate tracks.json file

---
+++ audio/generate_json_from_s3_push.py
@@ -0,0 +1,27 @@
+import json
+import subprocess
+
+# Command line output
+# Execute the command and capture the output
+output = subprocess.check_output(["aws", "s3", "ls", "audiod"]).decode("utf-8")
+
+# Split the output into lines
+lines = output.strip().split('\n')
+
+# Extract the file names from each line and add prefix
+tracks = ["https://audiod.s3.eu-west-3.amazonaws.com/" + line.split()[-1] for line in lines]
+
+# Create the JSON structure
+data = {"tracks": tracks}
+
+# Convert the JSON structure to a string
+json_data = json.dumps(data, indent=4)
+
+# Print the JSON data
+print(json_data)
+
+# Write the JSON data to a file
+with open("tracks.json", "w") as file:
+    file.write(json_data)
+
+
